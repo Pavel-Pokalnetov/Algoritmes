3376b9cdc6dfd0c78a98419ca3f7390b681fd9b2
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-06-16 18:03:02 +0500

    mini fix

--- src/main/java/seminar2/HeapSort.java
+++ src/main/java/seminar2/HeapSort.java
@@ -16,22 +16,22 @@ public class HeapSort {
     }
 
     // Перестроение кучи
-    private static void heap_rebuilding(int[] arr, int n, int i) {
+    private static void heap_rebuilding(int[] array, int n, int i) {
         int root_element = i; //корень
         int left_element = 2 * i + 1; //левый элемент
         int right_element = 2 * i + 2; //правый элемент
 
-        if (left_element < n && arr[left_element] > arr[root_element]) root_element = left_element;
+        if (left_element < n && array[left_element] > array[root_element]) root_element = left_element;
 
-        if (right_element < n && arr[right_element] > arr[root_element]) root_element = right_element;
+        if (right_element < n && array[right_element] > array[root_element]) root_element = right_element;
 
         if (root_element != i) {
-            int swap = arr[i];
-            arr[i] = arr[root_element];
-            arr[root_element] = swap;
+            int swap = array[i];
+            array[i] = array[root_element];
+            array[root_element] = swap;
 
             // Рекурсивно перестраиваем кучу
-            heap_rebuilding(arr, n, root_element);
+            heap_rebuilding(array, n, root_element);
         }
     }
 }
