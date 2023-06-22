5e0981800c4b26fea6b686d79c2e4af6ee876b65
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-06-23 00:24:35 +0500

    LeftRebBlackTree готово  с проверкой

--- src/main/java/seminar4/Main.java
+++ src/main/java/seminar4/Main.java
@@ -1,5 +1,20 @@
 package seminar4;
 
 public class Main {
+    public static void main(String[] args) {
+        LeftRedBlackTree<Integer> tree = new LeftRedBlackTree<>();
+        tree.add(24);
+        tree.printTree();
+        tree.add(5);
+        tree.printTree();
+        tree.add(1);
+        tree.printTree();
+        tree.add(15);
+        tree.printTree();
+        tree.add(3);
+        tree.printTree();
+        tree.add(8);
+        tree.printTree();
 
+    }
 }
